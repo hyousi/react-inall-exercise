90a5c39abd2a4e7a01966edaa5ade273c6346a9d
Ze Dang <4649tz@gmail.com>
2020-08-13 20:14:19 +0800

    [zedang] feat: implement Home page functionality

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import heroImage from '../../images/hero-image.png';
+import calcImage from '../../images/calculator.png';
+import timerImage from '../../images/timer.png';
+import { Link } from 'react-router-dom';
 import '../styles/home.less';
 
+export default function Home() {
+  return (
+    <>
+      <div className='hero-image'>
+        <div>在线实用工具</div>
+        <img src={heroImage} style={{ visibility: 'hidden' }} />
+      </div>
 
-const Home = () => {
-  return (<div className="home">
-    This is home page
-  </div>);
-};
-
-export default Home;
+      <div className='row'>
+        <div>
+          <img src={calcImage} alt='calculator link' />
+          <div>
+            <Link to='/calc'>计算器</Link>
+          </div>
+        </div>
+        <div>
+          <img src={calcImage} alt='calculator link' />
+          <div>
+            <Link to='/timer'>计时器</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
