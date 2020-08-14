d8319d33b2a28c59a1c3473809ed21344f991db3
Ze Dang <4649tz@gmail.com>
2020-08-14 22:03:19 +0800

    [zedang] feat: implement calc logic

--- src/app/components/Calculator.js
+++ src/app/components/Calculator.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Row from './Row';
 
-export default function Calculator() {
-  return (
-    <div>
-      Calculator
-    </div>
-  )
+export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monitor: '',
+      buttonsText: [
+        '+',
+        '-',
+        '✕',
+        '7',
+        '8',
+        '9',
+        '4',
+        '5',
+        '6',
+        '1',
+        '2',
+        '3',
+        '0',
+        'Clear',
+        '=',
+      ],
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event, buttonText) {
+    switch (buttonText) {
+      case 'Clear':
+        this.setState({ monitor: '' });
+        break;
+      case '+' || '-' || '✕':
+        this.setState({ monitor: this.state.monitor + buttonText });
+        break;
+      default:
+        this.setState({ monitor: this.state.monitor + buttonText });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h1>在线计算器</h1>
+        <Row className='monitor'>{this.state.monitor}</Row>
+        <Row>
+          {this.state.buttonsText.map((buttonText) => (
+            <button
+              key={`button-${buttonText}`}
+              onClick={(e) => this.handleClick(e, buttonText)}>
+              {buttonText}
+            </button>
+          ))}
+        </Row>
+      </>
+    );
+  }
 }
