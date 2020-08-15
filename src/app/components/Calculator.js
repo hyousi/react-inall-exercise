a95089d98e4d9e6935a35d60247417bf458fe5c1
Ze Dang <4649tz@gmail.com>
2020-08-15 12:06:01 +0800

    [zedang] fix: calc logic

--- src/app/components/Calculator.js
+++ src/app/components/Calculator.js
@@ -6,7 +6,10 @@ export default class Calculator extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      monitor: '',
+      operandLeft: '',
+      operandRight: '',
+      operator: '',
+      result: '',
       buttonsText: [
         '+',
         '-',
@@ -52,29 +55,50 @@ export default class Calculator extends Component {
   }
 
   handleClick(event, buttonText) {
+    const { operandLeft, operator, operandRight } = this.state;
     switch (buttonText) {
       case 'Clear':
-        this.setState({ monitor: '' });
+        this.setState({ operandLeft: '', operandRight: '', operator: '', result: '' });
         break;
       case '=':
-        const { monitor } = this.state;
-        const result = eval(monitor);
-        this.setState({ monitor: result });
+        if (operandLeft && operator && operandRight) {
+          const result = eval(operandLeft + operator + operandRight);
+          this.setState({ result });
+        }
         break;
-      case '+' || '-' || '✕':
-        this.setState({ monitor: this.state.monitor + buttonText });
+      case '+':
+        if (operandLeft) {
+          this.setState({ operator: buttonText });
+        }
+        break;
+      case '-':
+        if (operandLeft) {
+          this.setState({ operator: buttonText });
+        }
+        break;
+      case '✕':
+        if (operandLeft) {
+          this.setState({ operator: '*' });
+        }
         break;
       default:
-        this.setState({ monitor: this.state.monitor + buttonText });
+        if (!operator) {
+          this.setState({ operandLeft: operandLeft + buttonText });
+        } else {
+          this.setState({ operandRight: operandRight + buttonText });
+        }
     }
   }
 
   render() {
+    const {operandLeft, operator, operandRight, result} = this.state;
     return (
       <>
         <h1>在线计算器</h1>
         <div className='calc'>
-          <div className='calc-monitor'>{this.state.monitor}</div>
+          <div className='calc-monitor'>
+            {result ? result : operandLeft + operator + operandRight}
+          </div>
           <div className='calc-buttons'>
             {this.state.buttonsText.map((buttonText) => (
               <button
